00aca9ff40b9e3cb614416cf13e6ea9d329914af
Alex Mehr <alirezaalipouri@gmail.com>
2018-08-22 11:08:31 -0700

    Mongo Project

---
+++ server.js
@@ -0,0 +1,43 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+
+var db = require("./models");
+
+var PORT = 3000;
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/scrape", function (req, res) {
+    axios.get("http://www.echojs.com/").then(function (response) {
+        var $ = cheerio.load(response.data);
+        $("article h2").each(function (i, element) {
+            
+            var result = {};
+
+            result.title = $(this)
+                .children("a")
+                .text();
+            result.link = $(this)
+                .children("a")
+                .attr("href");
+
+            db.Article.create(result)
+                .then(function (dbArticle) {
+                 
+                    console.log(dbArticle);
+                })
+                .catch(function (err) {
+                  
+                    return res.json(err);
+                });
+        });
+    });
